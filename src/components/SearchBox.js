742d9f3c89588702506a2d805303bd747809ba83
Annanay032 <73812102+Annanay032@users.noreply.github.com>
2023-02-24 16:04:42 +0530

    d

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import axios from '../../node_modules/axios/index';
+import React, { useState } from 'react';
 
 // import React, { useEffect, useState } from "react";
 // import { Link } from "react-router-dom";
